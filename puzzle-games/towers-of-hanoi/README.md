4bfaa03f5be5ebeec9c9897b23b8c8cffcc715f6
Jake Shams <jshams62@gmail.com>
2020-05-21 17:19:26 -0700

    towers of hanoi mini-project

---
+++ puzzle-games/towers-of-hanoi/README.md
@@ -0,0 +1,12 @@
+# Towers of Hanoi Solver
+Tower of Hanoi is a mathematical puzzle where we have three rods and n disks. The objective of the puzzle is to move the entire stack to another rod, obeying the following simple rules:
+1) Only one disk can be moved at a time.
+2) Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack i.e. a disk can only be moved if it is the uppermost disk on a stack.
+3) No disk may be placed on top of a smaller disk.
+
+Here's an illustration for 3 disks:  
+<img src="https://media.geeksforgeeks.org/wp-content/uploads/tower-of-hanoi.png" width="50%"/>
+
+
+## Resources:
+[Geeks for Geeks - C Program for towers of hanoi](https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/)
---
+++ puzzle-games/towers-of-hanoi/towers_of_hanoi.py
@@ -0,0 +1,58 @@
+seen = {}
+
+
+def towers_of_hanoi_moves(n):
+    '''memoized towers of hanoi'''
+    if n == 1:
+        return [(1, 3)]
+    else:
+        if n not in seen:
+            toh_n_minus_one = towers_of_hanoi_moves(n - 1)
+            pivoted = pivot_twice(toh_n_minus_one)
+            seen[n] = pivoted[0] + [(1, 3)] + pivoted[1]
+        return seen[n]
+
+
+def pivot(lst_of_tuples, switch1, switch2):
+    return [swap(tup, switch1, switch2) for tup in lst_of_tuples]
+
+
+def pivot_twice(lst_of_tuples):
+    p1 = [swap(tup, 2, 3) for tup in lst_of_tuples]
+    p2 = [swap(tup, 1, 2) for tup in lst_of_tuples]
+    return p1, p2
+
+
+def swap(tup, switch1, switch2):
+    if tup[0] == switch1:
+        index0 = switch2
+    elif tup[0] == switch2:
+        index0 = switch1
+    else:
+        index0 = tup[0]
+    if tup[1] == switch1:
+        index1 = switch2
+    elif tup[1] == switch2:
+        index1 = switch1
+    else:
+        index1 = tup[1]
+    return (index0, index1)
+
+
+if __name__ == '__main__':
+    import sys
+    n = 5
+    if len(sys.argv) > 1:
+        n = int(sys.argv[1])
+    else:
+        n = input("Enter the number of rings you'd like to solve for: ")
+        try:
+            n = int(n)
+        except:
+            exit()
+    z = towers_of_hanoi_moves(n)
+    print(
+        f'With {n} rings in our tower the minumum number of moves is: {len(z)}')
+    print('And the moves are:')
+    # [print(f'{i + 1}. {y}') for i, y in enumerate(z)]
+    print(z)
